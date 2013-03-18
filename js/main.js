52db5dbb0836a728142402702f28d72f5be1e12d
bmac <bmac325@gmail.com>
2013-03-17 21:12:01 -0400

    some experimentation with getusermedia in chrome

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+
+var onSuccess = function(stream) {
+    var video = document.getElementById('video');
+    var audio = document.getElementById('audio');
+    video.src = window.URL.createObjectURL(stream);
+    // audio.src = window.URL.createObjectURL(stream);
+    
+};
+
+var onError = function() {
+    console.log('an error occured ' , arguments);
+};
+
+navigator.webkitGetUserMedia({video: true, audio: true}, onSuccess, onError);
