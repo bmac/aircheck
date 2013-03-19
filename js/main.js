709c5c253df0adf903e25eedd5dee254a750716b
bmac <bmac325@gmail.com>
2013-03-18 20:29:19 -0400

    remove audio tag

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@
 
 var onSuccess = function(stream) {
     var video = document.getElementById('video');
-    var audio = document.getElementById('audio');
     video.src = window.URL.createObjectURL(stream);
-    // audio.src = window.URL.createObjectURL(stream);
     
 };
 
@@ -13,3 +11,4 @@ var onError = function() {
 };
 
 navigator.webkitGetUserMedia({video: true, audio: true}, onSuccess, onError);
+
