47d9cecf08b3d86c5ff0ffc151f776072169bdd8
bmac <bmac325@gmail.com>
2013-03-23 19:01:10 -0400

    wrap main in a function to prevent leaking variables into the global scope

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
+(function() {
 
+    var onSuccess = function(stream) {
+        var video = document.getElementById('video');
+        video.src = window.URL.createObjectURL(stream);
 
-var onSuccess = function(stream) {
-    var video = document.getElementById('video');
-    video.src = window.URL.createObjectURL(stream);
-    
-};
+    };
 
-var onError = function() {
-    console.log('an error occured ' , arguments);
-};
+    var onError = function() {
+        console.log('an error occured ' , arguments);
+    };
 
-navigator.webkitGetUserMedia({video: true, audio: true}, onSuccess, onError);
+    navigator.webkitGetUserMedia({video: true, audio: true}, onSuccess, onError);
 
-var webrtc = new WebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remotesVideos',
-    // immediately ask for camera access
-    autoRequestMedia: true
-});
+    var webrtc = new WebRTC({
+        // the id/element dom element that will hold "our" video
+        localVideoEl: 'localVideo',
+        // the id/element dom element that will hold remote videos
+        remoteVideosEl: 'remotesVideos',
+        // immediately ask for camera access
+        autoRequestMedia: true
+    });
 
-// we have to wait until it's ready
-webrtc.on('readyToCall', function () {
-    // you can name it anything
-    webrtc.joinRoom('test room 12345 test test');
-});
+    // we have to wait until it's ready
+    webrtc.on('readyToCall', function () {
+        // you can name it anything
+        webrtc.joinRoom('test room 12345 test test');
+    });
+
+}());
