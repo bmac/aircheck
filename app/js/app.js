84107418f75b5f7b78647d5b183b5d7988517a0c
bmac <bmac325@gmail.com>
2013-08-07 17:48:13 -0400

    add webrtc.io back in because I can't make up my mind

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,15 @@
 var App = Ember.Application.create();
+// var peer = App.peer = new Peer({key: '5u88fz5wmt7nl8fr'});
 
 App.Router.map(function() {
     // put your routes here
-    this.route('room', { path: '/room/:roomName' });
+    this.resource('room', { path: '/room/:room_name' });
 });
 var l = Ember.HashLocation.create();
 
 App.IndexController = Ember.Controller.extend({
     goToRoom: function() {
-        l.setURL('room/' + this.get('roomName'));
+        this.transitionToRoute('room', {roomName: this.get('roomName')});
     }
 });
 
@@ -16,7 +17,7 @@ App.IndexController = Ember.Controller.extend({
 App.RoomRoute = Ember.Route.extend({
     model: function(params) {
         return {
-            roomName: params.roomName
+            roomName: params.room_name
         };
     }
 });
@@ -26,13 +27,34 @@ App.RoomController = Ember.ObjectController.extend({
     init: function() {
         var self = this;
 
-        
+        rtc.createStream({video: true, audio:true}, function(stream){
+            var objUrl = window.URL.createObjectURL(stream);
+            self.set('videoSrc', objUrl);
+        });
+
+        rtc.on('add remote stream', function(stream, socketId) {
+            var otherVideos = self.get('otherVideos');
+            otherVideos.pushObject({
+                stream: stream,
+                socketId: socketId,
+                videoSrc: URL.createObjectURL(stream)
+                //dataChannel: rtc.createDataChannel(socketId)
+            });
+            self.set('otherVideos', otherVideos);            
+        });
+
+        rtc.on('disconnect stream', function(socketId) {
+            var otherVideos = self.get('otherVideos').reject(function(stream) {
+                return stream.socketId = socketId;
+            });
+            self.set('otherVideos', otherVideos);
+        });
         window.c = this;
     },
     roomNameObserver: function() {
         // do not join an empty room
         if (this.get('roomName')) {
-            
+            rtc.connect('ws://localhost:8001/', this.get('roomName'));
         }
     }.observes('roomName')
 });
