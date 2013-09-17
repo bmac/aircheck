a81fec22ebc07171ecc59d6d7f60ead55e69e6e3
bmac <bmac325@GMail.com>
2013-09-17 10:59:29 -0400

    100% test coverage

--- tests/acceptance/index_test.js
+++ tests/acceptance/index_test.js
@@ -8,9 +8,17 @@ module("Acceptances - Index", {
 });
 
 test("index renders", 1, function(){
-  // expect(3);
-
   visit('/').then(function(){
-    ok(exists("label[for='roomName']:contains('Enter room name:')"));
+    ok(exists("h1:contains('Welcome to Aircheck')"));
+  });
+});
+
+test("goToRoom action navigates to the room route", 2, function(){
+  visit('/')
+  .fillIn(".room-name", "Room Name")
+  .click(".join").then(function() {
+      var activeTransition = App.__container__.lookup('router:main').router.activeTransition;
+      equal(activeTransition.targetName, 'room');
+      equal(activeTransition.params.room_name, 'Room Name');
   });
 });
