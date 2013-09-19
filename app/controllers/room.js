2c88eef68da74e877bf171ab8261c17737e33139
bmac <bmac325@GMail.com>
2013-09-19 18:58:04 -0400

    allow the user to change their nick by clicking on the nick name

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -5,8 +5,21 @@ var RoomController = Ember.ObjectController.extend({
             var room = this.get('model');
             room.send(msg);
             this.set('message', '');
+        },
+        toggleEditNick: function() {
+            this.toggleProperty('editNick');
+            console.log('editNick', this.get('editNick'));
+        },
+        saveNick: function(newNick) {
+            var room = this.get('model');
+            room.setNick(newNick);
+            this.set('newNick', '');
+            this.set('editNick', false);
         }
-    }
+    },
+    editNick: false,
+    msg: '',
+    newNick: ''
 });
 
 export default RoomController;
