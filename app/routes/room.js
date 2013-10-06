f448298398924e178194382276f9eb3badc2f21f
bmac <bmac325@GMail.com>
2013-10-06 19:24:07 -0400

    fix typo in css class name

--- app/routes/room.js
+++ app/routes/room.js
@@ -1,6 +1,7 @@
 import chat from 'aircheck/services/chat';
 import nameGen from 'aircheck/services/nameGen';
 import selectInput from 'aircheck/helpers/selectInput';
+import messageTime from 'aircheck/helpers/messageTime';
 
 var RoomRoute = Ember.Route.extend({
     model: function(params) {
