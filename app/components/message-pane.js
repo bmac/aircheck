fee477905eda6a400c2b36b4ae100a4cffc31cbc
bmac <bmac325@GMail.com>
2013-10-06 20:41:32 -0400

    use flexbox magic to align the input button with the bottom of the page

--- app/components/message-pane.js
+++ app/components/message-pane.js
@@ -1,4 +1,5 @@
 var MessagePaneComponent = Ember.Component.extend({
+    classNames: ['ember-view', 'message-pane'],
     chatMessages: Ember.computed.filter('messages', function(message) {
         var type = message.type;
         return type === 'MSG' || type === 'NICK';
